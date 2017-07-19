d9751c5d9c487403cc4da89be6a23c87d1256af8
Fabio Spampinato <spampinabio@gmail.com>
2017-07-19 08:27:15 +0200

    Fixed a require path

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,12 @@
 
 import * as _ from 'lodash';
 import {argv} from 'yargs';
+import * as path from 'path';
 import Utils from './utils';
 
 /* CONFIG */
 
-let config = Utils.require.json ( 'config.json' );
+let config = Utils.require.json ( path.join ( __dirname, '../../config.json' ) );
 
 if ( argv.config ) {
 
