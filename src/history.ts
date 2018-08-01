d42d33ada5980e2ab7481ee5b425fd0832e3a32e
Fabio Spampinato <spampinabio@gmail.com>
2018-08-01 20:25:15 +0200

    Retrieving the previous tokens more defensively

--- src/history.ts
+++ src/history.ts
@@ -43,7 +43,7 @@ const History = {
 
     if ( !history[url] || !history[url].length ) return;
 
-    return _.last ( history[url] );
+    return _.findLast ( history[url], val => val && val['tokens'] );
 
   }
 
